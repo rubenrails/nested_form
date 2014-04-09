7e4e8cd411ece8c6f9a4ce1249fdc3851a0bd16c
Ruben Ascencio <galateaweb@gmail.com>
2014-04-08 23:57:18 -0700

    Support for `bootstrap_form`
    Had to override main halter cause the original
    has options[:builder] hardcoded into the method.

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -36,6 +36,15 @@ module NestedForm
       end
     end
 
+    if defined?(NestedForm::BootstrapFormBuilder)
+      def bootstrap_nested_form_for(*args, &block)
+        options = args.extract_options!.reverse_merge(:builder => NestedForm::BootstrapFormBuilder)
+        custom_bootstrap_form_for(*(args << options)) do |f|
+          capture(f, &block).to_s << after_nested_form_callbacks
+        end
+      end
+    end
+
     def after_nested_form(association, &block)
       @associations ||= []
       @after_nested_form_callbacks ||= []
@@ -44,6 +53,26 @@ module NestedForm
         @after_nested_form_callbacks << block
       end
     end
+
+    def custom_bootstrap_form_for(object, options = {}, &block)
+
+      layout = case options[:layout]
+        when :inline
+          "form-inline"
+        when :horizontal
+          "form-horizontal"
+      end
+
+      if layout
+        options[:html] ||= {}
+        options[:html][:class] = [options[:html][:class], layout].compact.join(" ")
+      end
+
+      temporarily_disable_field_error_proc do
+        form_for(object, options, &block)
+      end
+    end
+
 
     private
       def after_nested_form_callbacks
